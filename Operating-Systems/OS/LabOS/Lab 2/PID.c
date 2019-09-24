2b5b73c48bdcc92f52ab13d744942b2a5339af50
danielroa98 <daniel.roa98@gmail.com>
2019-09-23 22:32:52 -0500

    Program used to view parent processes and their children

---
+++ Operating-Systems/OS/LabOS/Lab 2/PID.c
@@ -0,0 +1,48 @@
+/***********************************************************
+ *
+ *  Luis Daniel Roa : A01021960
+ *
+ *  Program to run Linux based terminal commands
+ *
+ **************************************************************/
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <sys/wait.h>
+ #include <sys/types.h>
+
+ int main() {
+
+   pid_t anakin = getpid();
+
+   printf("Luke, I am your father, I am: %d\n", anakin);
+
+   sleep(30);
+
+   pid_t luke = fork();
+
+   if (luke > 0) {
+
+     printf("I %d, am your father\n", anakin);
+
+     printf("Noooo, replied %d\n", luke);
+
+     wait(NULL);
+
+
+   }
+
+   pid_t leia = fork();
+
+   if (leia > 0) {
+
+     printf("Hey I'm your sister %d, remember %d?\n", leia, luke);
+
+     printf("My father is %d\n", anakin);
+
+     sleep(100);
+
+   }
+
+   return 0;
+ }
